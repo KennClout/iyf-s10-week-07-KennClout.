b647195bb74f061a3ec919331be5f809397bfdd6
Kennclout <kennclout@gmail.com>
2026-05-16 02:25:35 +0300

    Implement state management with save and load functions

--- js/state.js
+++ js/state.js
@@ -1,1 +1,18 @@
 
+import { save, load } from "./storage.js";
+
+const STORAGE_KEY = "todos";
+const FILTER_KEY = "filter";
+const THEME_KEY = "theme";
+
+export const state = {
+    todos: load(STORAGE_KEY, []),
+    filter: load(FILTER_KEY, "all"),
+    theme: load(THEME_KEY, "light")
+};
+
+export function saveState() {
+    save(STORAGE_KEY, state.todos);
+    save(FILTER_KEY, state.filter);
+    save(THEME_KEY, state.theme);
+}
